6e20b8c820b772fd6cd7c11a20b58f20e778628a
00jacs <gillbatesdev@gmail.com>
2024-10-12 16:39:13 +0200

    feat(ux, dx): minor cleanup, improve logging and UX, getting keys

--- src/logger.rs
+++ src/logger.rs
@@ -1,17 +1,52 @@
 use colored::*;
 
-pub fn info(message: &str) {
-    println!("{}", message.blue());
+/// Prints a debug message (level: 4) in the same line
+pub fn debugsn(message: &str) {
+    print!("{}", message.bright_purple());
 }
 
+/// Prints a debug message (level: 4) in a new line
+pub fn debug(message: &str) {
+    println!("{}", message.bright_purple());
+}
+
+/// Prints a info message (level: 3) in the same line
+pub fn infosn(message: &str) {
+    print!("{}", message.normal());
+}
+
+/// Prints an info message (level: 3) in a new line
+pub fn info(message: &str) {
+    println!("{}", message.normal());
+}
+
+/// Prints a warn message (level: 2) in the same line
+pub fn warnsn(message: &str) {
+    print!("{}", message.yellow());
+}
+
+/// Prints an warn message (level: 2) in a new line
 pub fn warn(message: &str) {
     println!("{}", message.yellow());
 }
 
+/// Prints an error message (level: 1) in the same line
+pub fn errorsn(message: &str) {
+    print!("{}", message.red());
+}
+
+/// Prints an error message (level: 1) in a new line
 pub fn error(message: &str) {
     println!("{}", message.red());
 }
 
+/// Prints a success message (level: 1) in the same line
+pub fn successsn(message: &str) {
+    print!("{}", message.green());
+}
+
+/// Prints an success message (level: 1) in a new line
 pub fn success(message: &str) {
     println!("{}", message.green());
 }
+
--- src/main.rs
+++ src/main.rs
@@ -3,48 +3,29 @@ mod logger;
 use regex::Regex;
 use std::collections::HashMap;
 use std::fs::{self, OpenOptions, File, read_to_string};
-use std::io::{self, stdout, stdin, Read, Write};
-use std::path::{Path, PathBuf};
+use std::io::{self, stdout, stdin, Write};
+use std::path::{Path};
 
 use clipboard::{ClipboardContext, ClipboardProvider};
-use clap::Parser;
 
-/// The default path to the main safe storage
+/// The default path to the main safe storage.
 const SAFE_CONTENT_PATH: &str = "./.safe/.main.safe";
 
-/// Search for a pattern in the stored passwords
-#[derive(Parser,)]
-struct Cli {
-    /// Which string are you looking for?
-    #[arg(short, long, default_value_t=String::from(""))]
-    pattern: String,
-
-    /// Command which you want to run
-    #[arg(short, long, default_value_t=String::from("search"))]
-    command: String,
-
-    /// Key for the password/website
-    #[arg(short, long, default_value_t=String::from(""))]
-    key: String,
-
-    /// Value of the password, this will get encrypted
-    #[arg(long, default_value_t=String::from(""))]
-    password: String
-}
-
-fn handle_init_safe(file_path: &Path) -> io::Result<(String)> {
+/// Initializes the .safe directory (creates the dir and the .main.safe file.)
+fn handle_init_safe(file_path: &Path) -> io::Result<String> {
     logger::info("Initializing your first safe.");
 
     if let Some(parent) = file_path.parent() {
         fs::create_dir_all(parent)?;
     }
 
-    let file = File::create(&file_path)?;
+    let _file = File::create(&file_path)?;
 
     logger::success("Your safe has been initialized successfully!");
-    Ok((String::from("Safe initialized")))
+    Ok(String::from("Safe initialized"))
 }
 
+/// A utility method which opens the file in append mode and adds a new line with "content".
 fn add_line_to_file(file_path: &Path, content: &str) -> io::Result<()> {
     let mut file = OpenOptions::new()
         .create(true)
@@ -56,11 +37,16 @@ fn add_line_to_file(file_path: &Path, content: &str) -> io::Result<()> {
     Ok(())
 }
 
+/// A method which reads the .main.safe file and returns all the key/value pairs detected
+/// (encrypted.)
 fn get_all_safe_keys(file_path: &Path) -> HashMap<String, String> {
     let content = read_to_string(file_path).expect("Unable to read file");
     let mut keys_map = HashMap::new();
+    let mut line_number = 0;
 
     for line in content.lines() {
+        line_number += 1;
+
         if line.trim().is_empty() {
             continue;
         }
@@ -71,17 +57,37 @@ fn get_all_safe_keys(file_path: &Path) -> HashMap<String, String> {
 
             keys_map.insert(key, value);
         } else {
-            eprintln!("Warning: Invalid line format: {}", line);
+            logger::warn(&format!("Warning: invalid line format at line number: {}. Please investigate or contact support.", line_number));
         }
     }
 
-    println!("Returning keys map: {:?}", keys_map);
+    logger::debug(&format!("Returning keys map: {:?}", keys_map));
     keys_map
 }
 
-fn handle_search(keys_map: HashMap<String, String>, key_pattern: String) {
-    println!("Looking for password of pattern: {}", key_pattern.to_string());
+fn handle_single_match(key: &str, password: &str) {
+    logger::infosn(&format!("Found one key '{}'. Do you want to retrieve the password? (y/N): ", key));
+    stdout().flush().expect("Failed to flush stdout");
 
+    let mut input = String::new();
+    stdin()
+        .read_line(&mut input)
+        .expect("Failed to read input");
+
+    let input = input.trim().to_lowercase();
+    if input == "y" || input == "yes" || input == "Yes" || input == "Y" {
+        if copy_to_clipboard(password).is_ok() {
+            logger::success(&format!("Password for '{}' has been copied to clipboard.", key));
+        }
+    } else {
+        logger::info("Password retrieval canceled.");
+        std::process::exit(1);
+    }
+}
+
+/// This method handles the "search" command; we display all the found keys matching the user
+/// phrase and we prompt the user to choose until there is only one left.
+fn handle_search(keys_map: HashMap<String, String>, key_pattern: String) {
     let re = Regex::new(&key_pattern).expect("Invalid regex pattern");
     let matching_keys: Vec<(&String, &String)> = keys_map
         .iter()
@@ -91,12 +97,13 @@ fn handle_search(keys_map: HashMap<String, String>, key_pattern: String) {
     let num_matches = matching_keys.len();
 
     if num_matches == 0 {
-        println!("No matching keys found.");
-    } else if num_matches == 1 {
-        let (key, password) = matching_keys[0];
+        logger::warn("No matching keys found. Here's a list of all the available keys that you saved:");
 
-        println!("Found one matching key: '{}'", key);
-        print!("Do you want to retrieve the password? (y/N): ");
+        for (key, _) in &keys_map {
+            logger::info(&format!("\t- {}", key));
+        }
+
+        logger::infosn("Please provide a proper key to look for the password: ");
         stdout().flush().expect("Failed to flush stdout");
 
         let mut input = String::new();
@@ -104,23 +111,30 @@ fn handle_search(keys_map: HashMap<String, String>, key_pattern: String) {
             .read_line(&mut input)
             .expect("Failed to read input");
 
-        let input = input.trim().to_lowercase();
-        if input == "y" || input == "yes" || input == "Yes" || input == "Y" {
-            println!("Password for '{}': '{}'", key, password);
+        let input = input.trim();
+        handle_search(keys_map, input.to_string());
 
-            if copy_to_clipboard(password).is_ok() {
-                println!("Password for '{}' has been copied to clipboard", key);
-            }
-        } else {
-            println!("Password retrieval canceled.");
-        }
+        std::process::exit(1);
+    } else if num_matches == 1 {
+        let (key, password) = matching_keys[0];
+        handle_single_match(key, password);
     } else {
-        println!("Multiple matching keys found: ");
-        for (key, _) in &matching_keys {
-            println!("- {}", key);
+        // Let's find out whether there is an exact match
+        // if yes, handle single match
+        // if not, proceed with the multiple matching approach
+        for (key, pswd) in &matching_keys {
+            if **key == key_pattern {
+                handle_single_match(key, pswd);
+            }
         }
 
-        println!("Please provide a more specific pattern to narrow down the search.");
+        logger::info("Multiple matching keys found: ");
+        for (key, _) in &matching_keys {
+            logger::info(&format!("\t- {}", key));
+        }
+
+        logger::infosn("Please provide a more specific pattern to narrow down the search: ");
+        stdout().flush().expect("Failed to flush stdout");
 
         let mut input = String::new();
         stdin()
@@ -133,10 +147,9 @@ fn handle_search(keys_map: HashMap<String, String>, key_pattern: String) {
 }
 
 fn handle_add_key(file_path: &Path, key: String, password: String) {
-    println!("Adding key: {}", key);
-
     let encrypted_line = format!("{}={};", key, password);
     add_line_to_file(file_path, &encrypted_line);
+    logger::success(&format!("Successfully added encrypted password for key '{}' to your safe. You can now retrieve its value.", key));
 }
 
 fn copy_to_clipboard(text: &str) -> Result<(), Box<dyn std::error::Error>> {
@@ -146,35 +159,46 @@ fn copy_to_clipboard(text: &str) -> Result<(), Box<dyn std::error::Error>> {
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut args = Cli::parse();
-
-    if args.command == "search" && args.pattern.is_empty() {
-        args.pattern = std::env::args().nth(2).expect("no pattern given");
-    }
-
-    println!("command: {:?}, pattern: {:?}", args.command, args.pattern);
-
     let file_path = Path::new(SAFE_CONTENT_PATH);
 
     if !file_path.exists() {
-        println!("Creating path...");
         handle_init_safe(file_path);
     }
 
-    match String::from(args.command).as_str() {
-        "add" => {
-            println!("handle_add_key!");
-            handle_add_key(file_path, String::from(args.key), String::from(args.password))
-        },
-        "search" => {
-            let keys_map = get_all_safe_keys(file_path);
-            handle_search(keys_map, String::from(args.pattern));
-        },
-        _ => {
-            println!("Not matched...");
+    let first_arg = std::env::args().nth(1).unwrap_or_else(|| {
+        String::from("search")
+    });
+
+    let second_arg = std::env::args().nth(2).unwrap_or_else(|| {
+        String::from("")
+    });
+
+    let third_arg = std::env::args().nth(3).unwrap_or_else(|| {
+        String::from("")
+    });
+
+    if first_arg == "search" {
+        let keys_map = get_all_safe_keys(file_path);
+        handle_search(keys_map, String::from(second_arg));
+    } else if first_arg == "add" {
+        if second_arg.is_empty() {
+            logger::error("Key cannot be empty. Please give a correct 'safe add <key> <password>'");
+            std::process::exit(1);
         }
+
+        if third_arg.is_empty() {
+            logger::error("Password cannot be empty. Please give a correct 'safe add <key> <password>' command.");
+            std::process::exit(1);
+        }
+
+        handle_add_key(file_path, String::from(second_arg), String::from(third_arg))
+    }
+    // if no command provided, we assume that it's search and
+    // the second argument is the phrase we're looking for
+    else if first_arg != "search" && first_arg != "add" {
+        let keys_map = get_all_safe_keys(file_path);
+        handle_search(keys_map, String::from(first_arg));
     }
 
     Ok(())
 }
-
